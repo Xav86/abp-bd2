a2a728490fd3f8e375ceebfbdce0b4a98c0903d1
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2025-06-22 15:20:35 -0300

    Fix: Corrigido os scripts de DDL

--- scripts/ddl/tabelas/criacao_tabelas.sql
+++ scripts/ddl/tabelas/criacao_tabelas.sql
@@ -8,7 +8,7 @@ GO
 
 CREATE TABLE [Enderecos] (
   [id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
-  [id_usuario] int NOT NULL,
+  [id_pessoa] int NOT NULL,
   [logradouro] varchar(100) NOT NULL,
   [numero] varchar(10) NOT NULL,
   [complemento] varchar(50) NOT NULL,
@@ -19,20 +19,20 @@ CREATE TABLE [Enderecos] (
 )
 GO
 
-CREATE TABLE [Cliente_Fisico] (
-  [id_usuario] int PRIMARY KEY NOT NULL,
+CREATE TABLE [Clientes_Fisicos] (
+  [id_pessoa] int PRIMARY KEY NOT NULL,
   [cpf] char(11) UNIQUE NOT NULL
 )
 GO
 
-CREATE TABLE [Cliente_Juridico] (
-  [id_usuario] int PRIMARY KEY NOT NULL,
+CREATE TABLE [Clientes_Juridicos] (
+  [id_pessoa] int PRIMARY KEY NOT NULL,
   [cnpj] char(14) UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE [Funcionarios] (
-  [id_usuario] int PRIMARY KEY NOT NULL,
+  [id_pessoa] int PRIMARY KEY NOT NULL,
   [funcao] varchar(50) NOT NULL,
   [data_admissao] date NOT NULL,
   [ativo] bit NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE [Veiculos] (
   [marca] varchar(50) NOT NULL,
   [ano_fabricacao] char(4) NOT NULL,
   [chassi] char(17) NOT NULL,
-  [quilometragem] int NOT NULL
+  [quilometragem] smallint NOT NULL
 )
 GO
 
@@ -55,14 +55,7 @@ CREATE TABLE [Servicos] (
   [id] smallint PRIMARY KEY NOT NULL IDENTITY(1, 1),
   [nome] varchar(100) NOT NULL,
   [descricao] varchar(max) NOT NULL,
-  [preco_padrao] numeric(14,2) NOT NULL,
-  [tempo_estimado] smallint NOT NULL
-)
-GO
-
-CREATE TABLE [Status] (
-  [id] tinyint PRIMARY KEY NOT NULL IDENTITY(1, 1),
-  [descricao] varchar(50) NOT NULL
+  [preco_padrao] numeric(14,2) NOT NULL
 )
 GO
 
@@ -74,8 +67,8 @@ CREATE TABLE [Ordens_de_Servico] (
   [data_saida] date,
   [data_meta] date NOT NULL,
   [titulo] varchar(100) NOT NULL,
-  [descricao] varchar(max) NOT NULL,
-  [status_id] tinyint NOT NULL
+  [descricao] varchar(500) NOT NULL,
+  [status] tinyint NOT NULL
 )
 GO
 
@@ -85,7 +78,7 @@ CREATE TABLE [Funcionarios_OS] (
   [id_funcionario] int NOT NULL,
   [funcao_na_ordem] varchar(50) NOT NULL,
   [tempo_trabalhado] smallint NOT NULL,
-  [observacoes] varchar(max) NOT NULL
+  [observacoes] varchar(500) NOT NULL
 )
 GO
 
@@ -118,7 +111,7 @@ GO
 
 CREATE TABLE [Historico_Ordens] (
   [id] int PRIMARY KEY NOT NULL IDENTITY(1, 1),
-  [id_usuario] int NOT NULL,
+  [id_pessoa] int NOT NULL,
   [id_ordem_servico] int NOT NULL,
   [campo_alterado] varchar(100),
   [valor_anterior] varchar(max) NOT NULL,
@@ -129,16 +122,22 @@ CREATE TABLE [Historico_Ordens] (
 )
 GO
 
-ALTER TABLE [Enderecos] ADD FOREIGN KEY ([id_usuario]) REFERENCES [Pessoas] ([id])
+CREATE TABLE [Status] (
+  [id] tinyint PRIMARY KEY NOT NULL IDENTITY(1, 1),
+  [descricao] varchar(50) NOT NULL
+)
 GO
 
-ALTER TABLE [Cliente_Fisico] ADD FOREIGN KEY ([id_usuario]) REFERENCES [Pessoas] ([id])
+ALTER TABLE [Enderecos] ADD FOREIGN KEY ([id_pessoa]) REFERENCES [Pessoas] ([id])
 GO
 
-ALTER TABLE [Cliente_Juridico] ADD FOREIGN KEY ([id_usuario]) REFERENCES [Pessoas] ([id])
+ALTER TABLE [Clientes_Fisicos] ADD FOREIGN KEY ([id_pessoa]) REFERENCES [Pessoas] ([id])
 GO
 
-ALTER TABLE [Funcionarios] ADD FOREIGN KEY ([id_usuario]) REFERENCES [Pessoas] ([id])
+ALTER TABLE [Clientes_Juridicos] ADD FOREIGN KEY ([id_pessoa]) REFERENCES [Pessoas] ([id])
+GO
+
+ALTER TABLE [Funcionarios] ADD FOREIGN KEY ([id_pessoa]) REFERENCES [Pessoas] ([id])
 GO
 
 ALTER TABLE [Ordens_de_Servico] ADD FOREIGN KEY ([id_veiculo]) REFERENCES [Veiculos] ([id])
@@ -147,13 +146,13 @@ GO
 ALTER TABLE [Ordens_de_Servico] ADD FOREIGN KEY ([id_cliente]) REFERENCES [Pessoas] ([id])
 GO
 
-ALTER TABLE [Ordens_de_Servico] ADD FOREIGN KEY ([status_id]) REFERENCES [Status] ([id])
+ALTER TABLE [Ordens_de_Servico] ADD FOREIGN KEY ([status]) REFERENCES [Status] ([id])
 GO
 
 ALTER TABLE [Funcionarios_OS] ADD FOREIGN KEY ([id_ordem_servico]) REFERENCES [Ordens_de_Servico] ([id])
 GO
 
-ALTER TABLE [Funcionarios_OS] ADD FOREIGN KEY ([id_funcionario]) REFERENCES [Funcionarios] ([id_usuario])
+ALTER TABLE [Funcionarios_OS] ADD FOREIGN KEY ([id_funcionario]) REFERENCES [Funcionarios] ([id_pessoa])
 GO
 
 ALTER TABLE [Itens_Servico_OS] ADD FOREIGN KEY ([id_ordem_servico]) REFERENCES [Ordens_de_Servico] ([id])
@@ -168,7 +167,7 @@ GO
 ALTER TABLE [Pecas_OS] ADD FOREIGN KEY ([id_peca]) REFERENCES [Pecas_Estoque] ([id])
 GO
 
-ALTER TABLE [Historico_Ordens] ADD FOREIGN KEY ([id_usuario]) REFERENCES [Pessoas] ([id])
+ALTER TABLE [Historico_Ordens] ADD FOREIGN KEY ([id_pessoa]) REFERENCES [Pessoas] ([id])
 GO
 
 ALTER TABLE [Historico_Ordens] ADD FOREIGN KEY ([id_ordem_servico]) REFERENCES [Ordens_de_Servico] ([id])
--- scripts/ddl/tabelas/deleta_tabelas.sql
+++ scripts/ddl/tabelas/deleta_tabelas.sql
@@ -1,3 +1,34 @@
+/* Drop Constraint */
+DECLARE @sql NVARCHAR(MAX) = '';
+
+SELECT @sql += 'ALTER TABLE [' + OBJECT_SCHEMA_NAME(parent_object_id) + '].[' + OBJECT_NAME(parent_object_id) + '] DROP CONSTRAINT [' + name + '];' + CHAR(13)
+FROM sys.foreign_keys
+WHERE name IN (
+    SELECT fk.name
+    FROM sys.foreign_keys fk
+    JOIN sys.tables t ON fk.parent_object_id = t.object_id    WHERE t.name IN (
+        'Pessoas',
+        'Enderecos',
+        'Clientes_Fisicos',
+        'Clientes_Juridicos',
+        'Funcionarios',
+        'Historico_Ordens',
+        'Servicos',
+        'Pecas_Estoque',
+        'Status',
+        'Veiculos',
+        'Ordens_de_Servico',
+        'Funcionarios_OS',
+        'Itens_Servico_OS',
+        'Pecas_OS'
+    )
+);
+
+EXEC sp_executesql @sql;
+
+
+
+
 DROP TABLE IF EXISTS [Historico_Ordens];
 DROP TABLE IF EXISTS [Pecas_OS];
 DROP TABLE IF EXISTS [Itens_Servico_OS];
@@ -7,8 +38,8 @@ DROP TABLE IF EXISTS [Veiculos];
 DROP TABLE IF EXISTS [Servicos];
 DROP TABLE IF EXISTS [Pecas_Estoque];
 DROP TABLE IF EXISTS [Funcionarios];
-DROP TABLE IF EXISTS [Cliente_Juridico];
-DROP TABLE IF EXISTS [Cliente_Fisico];
+DROP TABLE IF EXISTS [Clientes_Juridicos];
+DROP TABLE IF EXISTS [Clientes_Fisicos];
 DROP TABLE IF EXISTS [Enderecos];
 DROP TABLE IF EXISTS [Pessoas];
 DROP TABLE IF EXISTS [Status];
