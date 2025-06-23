9aec5840a4b3189e3789e25a31a0615eae141db2
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2025-06-23 00:10:39 -0300

    feat pergunta 6

---
+++ scripts/dql/pergunta_6.sql
@@ -0,0 +1,27 @@
+SELECT 
+    pe.nome AS nome_peca,
+    COUNT(pos.id) AS vezes_utilizada,
+    SUM(pos.quantidade) AS quantidade_total,
+    SUM(pos.quantidade * pos.preco_unitario) AS valor_total_gerado,
+    'Últimos 30 dias' AS periodo
+FROM Pecas_OS pos
+JOIN Pecas_Estoque pe ON pe.id = pos.id_peca
+JOIN Ordens_de_Servico os ON os.id = pos.id_ordem_servico
+WHERE os.data_entrada >= DATEADD(DAY, -30, GETDATE())
+GROUP BY pe.nome
+
+UNION ALL
+
+SELECT 
+    pe.nome AS nome_peca,
+    COUNT(pos.id) AS vezes_utilizada,
+    SUM(pos.quantidade) AS quantidade_total,
+    SUM(pos.quantidade * pos.preco_unitario) AS valor_total_gerado,
+    'Últimos 90 dias' AS periodo
+FROM Pecas_OS pos
+JOIN Pecas_Estoque pe ON pe.id = pos.id_peca
+JOIN Ordens_de_Servico os ON os.id = pos.id_ordem_servico
+WHERE os.data_entrada >= DATEADD(DAY, -90, GETDATE())
+GROUP BY pe.nome
+
+ORDER BY periodo, quantidade_total DESC;
